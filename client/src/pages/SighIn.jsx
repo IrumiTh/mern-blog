80d70aace0bb7368a4a0f4be14784be0910feacf
irumi <irumiabeywickrama@gmail.com>
2024-04-25 21:59:53 +0530

    add redux toolkit

--- client/src/pages/SighIn.jsx
+++ client/src/pages/SighIn.jsx
@@ -2,11 +2,13 @@ import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useState } from 'react';
+import {useDispatch,useSelector} from 'react-redux';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
 
 export default function SingIn() {
   const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const {loading, error: errorMessage} = useSelector( state => state.user);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const handelChange= (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
@@ -14,34 +16,31 @@ export default function SingIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!formData.email || !formData.password) {
-      return setErrorMessage('Please fill out all fields');
+      return dispatch(signInFailure('please fill all the fields'));
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
       });
-      if (!res.ok) {
-        throw new Error('Signup failed');
-      }else{
-        navigate('/');
-      }
+      
       const data = await res.json();
       if (data.success === false) {
         
-        return setErrorMessage(data.message);
+        dispatch(signInFailure(data.message));
         
       }
       console.log(data); // Log response data
-      setLoading(false);
+      if (res.ok) {
+      
+        dispatch(signInSuccess(data));
+        navigate('/');
+      }
       
     } catch (error) {
-      console.error('Error signing up:', error.message);
-      setErrorMessage(error.message);
-      setLoading(false);
+      dispatch(signInFailure(error.message))
     }
   };
   
