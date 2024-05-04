c70aa8825fe2c1b0bb54db5ebd5d04a3ad288f06
irumi <irumiabeywickrama@gmail.com>
2024-05-04 23:12:37 +0530

    add comment section to the post page

--- api/index.js
+++ api/index.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
-import postRouter from './routes/post.route.js'
+import postRouter from './routes/post.route.js';
+import commentRoutes from './routes/comment.route.js'
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 dotenv.config();
@@ -35,6 +36,7 @@ app.listen(3000, () => {
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/post', postRouter);
+app.use('/api/comment', commentRoutes);
 
 
 app.use((err, req, res, next ) =>{
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import OnlyAdminPrivateroute from './components/OnlyAdminPrivateroute'
 import CreatePost from './pages/CreatePost'
 import UpdatePost from './pages/Update'
 import PostPage from './pages/PostPage'
+import ScrollToTop from './components/ScrollToTop'
 
 
 
@@ -21,6 +22,7 @@ import PostPage from './pages/PostPage'
 export default function App() {
   return (
     <BrowserRouter >
+    <ScrollToTop />
     <Header/>
       <Routes>
         <Route path="/" element={<Home/>} />
